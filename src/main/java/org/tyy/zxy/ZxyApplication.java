ad9976564777a66975c8c0fe596df9599f91acfb
LXJZ <11698107+zhang-xinyu123456@user.noreply.gitee.com>
2026-03-29 00:28:41 +0800

    呢哇

---
+++ src/main/java/org/tyy/zxy/ZxyApplication.java
@@ -0,0 +1,13 @@
+package org.tyy.zxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tyy/zxy/ZxyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tyy.zxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
